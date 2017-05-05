28f8320b2701c3990557a1e0051358ac89609894
Koswan212 <CosOne212@gmail.com>
2017-05-05 13:22:49 +0700

    Create hook.php

---
+++ hook.php
@@ -0,0 +1,23 @@
+<?php
+define('HS', true);
+/*
+ *  Programmer  : Hasanudin HS
+ *  Email       : banghasan@gmail.com
+ *  Telegram    : @hasanudinhs
+ *
+ *  Name        : Template bot telegram - php
+ *  Fungsi      : Sample bot API
+ *  Pembuatan   : Mei 2016
+ *
+ *  File        : hook.php
+ *  Tujuan      : bot hook untuk telegram
+ *	Syarat		: hosting harus HTTPS bersertifikat
+ *  ____________________________________________________________
+*/
+require_once 'bot-api-config.php';
+require_once 'bot-api-fungsi.php';
+require_once 'bot-api-proses.php';
+$entityBody = file_get_contents('php://input');
+$message = json_decode($entityBody, true);
+prosesApiMessage($message);
+// Telegram by: banghasan @hasanudinhs @myqers;
