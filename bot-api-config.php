c284b97bba75c40efc882a7d2c1f50f420b5bd8d
Koswan212 <CosOne212@gmail.com>
2017-05-05 13:30:37 +0700

    Create bot-api-config.php

---
+++ bot-api-config.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('HS')) {
+    die('Tidak boleh diakses langsung.');
+}
+$token = '';
+$debug = true;
+$chatid = '213567634';
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2017 GitHub, Inc. Terms Privacy Security
